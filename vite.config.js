e209036d9ecf7a8d96171f79bb652c7111e7f71c
Ajay Gangwar <149433023+MrAjayGangwar2001@users.noreply.github.com>
2025-11-19 01:12:06 +0530

    Add allowed host for Vite server

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['portfolio-website-3j6p.onrender.com'],
     host: true, // allows LAN access
     port: 5173,
     base: './',
